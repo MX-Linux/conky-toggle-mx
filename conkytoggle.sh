6e52cdee867c3b0796e12d05d3dd2956febe786a
Dolphin Oracle <dolphinoracle@gmail.com>
2018-03-04 20:38:11 -0500

    add autostart to toggle get version number in line with other mx apps

--- conkytoggle.sh
+++ conkytoggle.sh
@@ -11,10 +11,12 @@ main()
 if pidof conky | grep [0-9] > /dev/null
  then
   killall conky 
+  autostart_off
  else
     test=$(grep -q "conky -c" /home/$USER/.conky/conky-startup.sh && echo $?)
     if [ "$test" = "0" ]; then
          launch_conky
+         autostart_on
     else
     conky-manager &
     fi
@@ -30,6 +32,24 @@ sed -i s/sleep.*/sleep\ 1s/ /home/$USER/.conky/conky-startup.sh
 sh /home/$USER/.conky/conky-startup.sh &
 
 sed -i s/sleep.*/sleep\ $SLEEP/ /home/$USER/.conky/conky-startup.sh
+}
+
+autostart_off()
+{
+    
+if [ -e /home/$USER/.config/autostart/conky.desktop ]; then 
+    sed -i -r s/Hidden=.*/Hidden=true/ /home/$USER/.config/autostart/conky.desktop
+fi
+
+}
+
+autostart_on()
+{
+    
+if [ -e /home/$USER/.config/autostart/conky.desktop ]; then 
+    sed -i -r s/Hidden=.*/Hidden=false/ /home/$USER/.config/autostart/conky.desktop
+fi
+
 }
 
 main
